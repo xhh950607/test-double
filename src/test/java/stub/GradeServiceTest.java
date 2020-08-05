fe04ba31bf8bc7f6f7ba93061b6cdaedf95a4085
xuhuihui <494655043@qq.com>
2020-08-05 09:57:23 +0800

    stub, using anotation

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -2,7 +2,12 @@ package stub;
 
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.Arrays;
 
@@ -12,12 +17,19 @@ public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
+    @Mock
+    private GradeSystem mockedGradeSystem;
+    @InjectMocks
+    private GradeService gradeService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        GradeSystem mockedGradeSystem = mock(GradeSystem.class);
-        when(mockedGradeSystem.gradesFor(1)).thenReturn(Arrays.asList(80.0,90.0,100.0));
-        GradeService gradeService = new GradeService(mockedGradeSystem);
+        when(mockedGradeSystem.gradesFor(1)).thenReturn(Arrays.asList(80.0, 90.0, 100.0));
         double result = gradeService.calculateAverageGrades(1);
         Assertions.assertEquals(90.0, result);
     }
